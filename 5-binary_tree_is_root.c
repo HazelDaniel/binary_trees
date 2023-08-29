67238fafa18aad5e6e2ae5598918ad85d6da2430
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-29 10:33:35 +0100

    submission for task 5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_is_root - a function that returns
+ * whether a node is the root of a binary tree
+ * @node: the node to check against
+ * Return: boolean
+ **/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	return (!(node->parent));
+}
