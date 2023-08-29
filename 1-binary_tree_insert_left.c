8ce386e31edd8f341c0fb99ed48f984f85d845be
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-29 09:31:20 +0100

    submission for task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_insert_left - a function that inserts
+ * a node to the left of an internal node
+ * @parent: the internal node that is bouta be a parent
+ * @value: the value that the newly created node is bouta
+ * have
+ * Return: binary_tree_t
+ **/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
+	if (new_node)
+		new_node->n = value;
+	if (parent)
+	{
+		parent->left = new_node;
+		new_node->parent = parent;
+	}
+	return (new_node);
+}
