59c96adb8b7f37b3e956bc0c1cdab58c92d8f5d8
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-29 22:32:51 +0100

    submission for task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 
 
+int bin_tree_is_full(const binary_tree_t *tree, int reset);
+
 /**
  * is_leaf - a function that returns whether
  * a node is a leaf
@@ -41,24 +43,51 @@ int has_even_children(const binary_tree_t *node)
  **/
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int is_full_here = 0;
+	int res_bool;
+
+	res_bool = bin_tree_is_full(tree, 0);
+	(void)bin_tree_is_full(NULL, 1);
+	return (res_bool);
+}
+
+/**
+ * bin_tree_is_full - a function that returns
+ * whether a binary tree is full or not
+ * @tree: the pointer to the root node of the binary
+ * @reset: this is to add state to the function. an augmentation
+ * added to provide correct output in an edge-case
+ * tree
+ * description: one way to find out is to
+ *             add all the total balance factors of
+ *             all nodes in the tree
+ * Return: void
+ **/
+int bin_tree_is_full(const binary_tree_t *tree, int reset)
+{
+	int is_full_here = 1;
+	static int count;
+
+	if (reset)
+	{
+		count = 0;
+		return (count);
+	}
 
 	if (!tree)
-		return (0);
-	if (is_leaf(tree))
 	{
-		if (!tree->parent)
-			return (1);
-		return (0);
+		if (!count)
+			return (0);
+		return (1);
 	}
-	if (!has_even_children(tree))
-	{
+
+	if (has_even_children(tree) == 1)
 		is_full_here = 1;
-	}
 	else
-	{
-		is_full_here += binary_tree_is_full(tree->left);
-		is_full_here += binary_tree_is_full(tree->right);
-	}
-	return (is_full_here == 0);
+		is_full_here = 0;
+
+	count++;
+	is_full_here = (is_full_here && bin_tree_is_full(tree->left, 0)
+	&& bin_tree_is_full(tree->right, 0));
+
+	return (is_full_here);
 }
