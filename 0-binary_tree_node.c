6c107feff2820c6283f58cf8c73cc883778215f0
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-29 09:24:15 +0100

    submission for task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,7 +14,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	if (!new_node)
 		return (NULL);
+	new_node->right = NULL;
+	new_node->left = NULL;
 	new_node->n = value;
+	new_node->parent = parent;
+	if (!parent)
+		return (new_node);
 	if (value > parent->n)
 		parent->right = new_node;
 	else
