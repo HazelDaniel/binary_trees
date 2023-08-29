69097f4e23fea26b809373caf6066cc8f260aa68
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-29 09:43:36 +0100

    submission for task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,11 +14,23 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
 	if (new_node)
-		new_node->n = value;
-	if (parent)
 	{
-		parent->left = new_node;
+		new_node->n = value;
 		new_node->parent = parent;
 	}
+	if (parent)
+	{
+		if (parent->left)
+		{
+			if (new_node)
+			{
+				parent->left->parent = new_node;
+				new_node->left = parent->left;
+			}
+		}
+		parent->left = new_node;
+	}
+	else
+		return (NULL);
 	return (new_node);
 }
