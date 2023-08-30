a4c3d2a9a3412aa689aee934200851e578d0de47
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-30 07:56:54 +0100

    submission for task 100

--- binary_trees.h
+++ binary_trees.h
@@ -22,23 +22,6 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 
-/**
- * struct b_tree_list - a linked list
- * containing pointer to nodes in a
- * binary trees due to some pre-computations
- *
- * @value: the pointer to the current
- * node in the list
- * @next: the pointer to the next
- * item in the linked list
- * @prev: the pointer to the previous item in the
- * linked list
- **/
-struct b_tree_list
-{
-	struct binary_tree_s *value;//DON'T FREE
-	struct b_tree_list *next;
-};
 
 /* TYPEDEFS */
 typedef struct binary_tree_s binary_tree_t;
@@ -49,10 +32,6 @@ typedef struct b_tree_list b_tree_list_t;
 
 /* UTILITY FUNCTIONS */
 void binary_tree_print(const binary_tree_t *tree);
-b_tree_list_t *create_b_tree_list(binary_tree_t * value);
-void append_b_tree_list(b_tree_list_t **head_ptr, binary_tree_t *node);
-void prepend_b_tree_list(b_tree_list_t **head_ptr, binary_tree_t *node);
-void free_b_tree_list(b_tree_list_t **head_ptr);
 
 /* CORE FUNCTIONS */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
