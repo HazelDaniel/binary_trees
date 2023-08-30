1f787b3dcbbe6e6854704398379d642e63bfa505
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-30 08:47:20 +0100

    submission for task 100

--- binary_trees.h
+++ binary_trees.h
@@ -5,8 +5,6 @@
 #include "stddef.h"
 #include <stdio.h>
 
-#define STRUCTTAG struct
-
 /* STRUCTURES */
 /**
  * struct binary_tree_s - Binary tree node
@@ -24,37 +22,16 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 
-/**
- * struct b_tree_list - a linked list
- * containing pointer to nodes in a
- * binary trees due to some pre-computations
- *
- * @value: the pointer to the current
- * node in the list
- * @next: the pointer to the next
- * item in the linked list
- * @prev: the pointer to the previous item in the
- * linked list
- **/
-STRUCTTAG b_tree_list
-{
-	struct binary_tree_s *value;//DON'T FREE
-	struct b_tree_list *next;
-};
 
 /* TYPEDEFS */
-typedef STRUCTTAG binary_tree_s binary_tree_t;
-typedef STRUCTTAG binary_tree_s bst_t;
-typedef STRUCTTAG binary_tree_s avl_t;
-typedef STRUCTTAG binary_tree_s heap_t;
-typedef STRUCTTAG b_tree_list b_tree_list_t;
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+typedef struct b_tree_list b_tree_list_t;
 
 /* UTILITY FUNCTIONS */
 void binary_tree_print(const binary_tree_t *tree);
-b_tree_list_t *create_b_tree_list(binary_tree_t * value);
-void append_b_tree_list(b_tree_list_t **head_ptr, binary_tree_t *node);
-void prepend_b_tree_list(b_tree_list_t **head_ptr, binary_tree_t *node);
-void free_b_tree_list(b_tree_list_t **head_ptr);
 
 /* CORE FUNCTIONS */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
